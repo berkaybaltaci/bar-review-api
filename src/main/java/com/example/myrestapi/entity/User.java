86f948a5b9c5a02400843957b90bf601ebb6b6de
berkaybaltaci <berkaybaltaci@gmail.com>
2022-04-08 00:00:03 +0300

    User entity added.

--- src/main/java/com/example/myrestapi/entity/User.java
+++ src/main/java/com/example/myrestapi/entity/User.java
@@ -1,7 +1,21 @@
 package com.example.myrestapi.entity;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
