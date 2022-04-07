69df5f4d26d372a07c3331c221b96bb7df6a73c6
berkaybaltaci <berkaybaltaci@gmail.com>
2022-04-08 01:01:21 +0300

    User and Review entities are updated. Database tables are also updated accordingly.

--- src/main/java/com/example/myrestapi/entity/User.java
+++ src/main/java/com/example/myrestapi/entity/User.java
@@ -1,15 +1,41 @@
 package com.example.myrestapi.entity;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "user")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
+
+
+
+    @OneToMany(mappedBy = "user", orphanRemoval = true)
+    private List<Review> reviews = new ArrayList<>();
+
+    @Column(name = "name")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
 
     public Long getId() {
         return id;
@@ -18,4 +44,6 @@ public class User {
     public void setId(Long id) {
         this.id = id;
     }
+
+
 }
