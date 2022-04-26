82d2faf8c0825437f925c8180b15270ce1c589fa
berkaybaltaci <berkaybaltaci@gmail.com>
2022-04-26 13:33:03 +0300

    Changed the main function name

--- src/main/java/com/example/myrestapi/BarReviewApplication.java
+++ src/main/java/com/example/myrestapi/BarReviewApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyRestApiApplication {
+public class BarReviewApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyRestApiApplication.class, args);
+        SpringApplication.run(BarReviewApplication.class, args);
     }
 
 }
